31d16fe82fe8739cf2f8639169a2f95c78639dab
Siddharth Vashishat <vashishatsiddharth@gmail.com>
2024-08-01 02:40:14 +0530

    Update FormField.jsx

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 
 const FormField = ({ LabeLName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
-  return (
-    <div>
+  return (   
+    <div> 
       <div className='flex items-center gap-2 mb-2'>
        <label 
        htmlFor={name}
-       className='block text-sm font-medium text-grey-900'
+       className='block text-sm font-medium text-[#efefef]'
        >
         {LabeLName}
         </label>
         {isSurpriseMe && (
-          <button
+          <button 
           type='button'
           onClick={handleSurpriseMe}
-          className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'
+          className='font-semibold text-xs bg-[#6469ff] py-1 px-2 rounded-[5px] text-[#efefef]'
           >
             Surprise Me  
           </button>
@@ -28,7 +28,7 @@ const FormField = ({ LabeLName, type, name, placeholder, value, handleChange, is
       value={value}
       onChange={handleChange}
       required
-      className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
+      className='bg-[#202123] border border-[#101014] text-white text-sm rounded-lg  focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
       />
     </div>
   )
